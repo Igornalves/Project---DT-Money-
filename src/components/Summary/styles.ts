0045098b07a76ab879e27cf6e778af93557b64fe
igor_alves <igornalves08@gmail.com>
2024-10-12 21:14:33 -0300

    colocando alguns ajustes  no projeto

--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -1,10 +1,38 @@
 import styled from "styled-components";
 
-export const SummaryConteiner = styled.div`
+export const SummaryConteiner = styled.section`
     width: 100%;
-    height: 1120px;
+    max-width: 1120px;
     margin: 0 auto;
     padding: 0 1.5rem;
     
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 2rem;
+
+    margin-top: -5rem;
+`
+
+interface SummaryCard {
+    variant?: 'green';
+}
+
+export const SummaryCard = styled.div`
+    background-color: ${props => props.theme['gray-600']};
+    border-radius: 6px;
+    padding: 2rem;
+
+    header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        color: ${props => props.theme['gray-300']};
+    }
+
+    strong {
+        display: block;
+        margin-top: 1rem;
+        font-size: 2rem;
+    }
 `
 
--- src/pages/Transactions/intex.tsx
+++ src/pages/Transactions/intex.tsx
@@ -1,9 +1,11 @@
 import Header from "../../components/Header/intex";
+import Summary from "../../components/Summary/intex";
 
 export default function Transactions() {
     return(
         <div>
             <Header/>
+            <Summary/>
         </div>
     );
 }
