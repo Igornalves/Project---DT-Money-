aebab833c15bf2bd6b13a4a197f9f060348a8339
igor_alves <igornalves08@gmail.com>
2024-10-14 23:19:10 -0300

    colocando as coisas no projeto de forma que tenha a Search no forms no projeto

---
+++ src/pages/Transactions/components/SearchForm/intex.tsx
@@ -0,0 +1,9 @@
+import { SearchFormConteiner } from "./styles";
+
+export default function Component() {
+    return(
+        <SearchFormConteiner>
+            <input type="text" placeholder="Buscar por transicoes "/>
+        </SearchFormConteiner>
+    );
+}
