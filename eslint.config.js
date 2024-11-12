0bb0e7f049c4b06df85207b2dd8410dc0a7b7aec
igor_alves <igornalves08@gmail.com>
2024-11-12 11:28:31 -0300

    criando novas altracoes para partes do lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,21 @@
-// eslint.config.js
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from '@typescript-eslint/eslint-plugin'; 
-import tsParser from '@typescript-eslint/parser';
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
 
-export default [
+export default tseslint.config(
+  { ignores: ['dist'] },
   {
-    ignores: ['dist'],
-  },
-  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-      parser: tsParser,
-      parserOptions: {
-        sourceType: 'module',
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
-      '@typescript-eslint': tseslint,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -35,10 +24,5 @@ export default [
         { allowConstantExport: true },
       ],
     },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
   },
-];
+)
